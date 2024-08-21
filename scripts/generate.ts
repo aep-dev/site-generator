1f287dbb8a4ecf83b6a0dda561af7540fb1d932b
Alex Stephen <astephen2@gmail.com>
2024-08-21 16:11:25 -0700

    write some files!

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,6 +1,15 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { load } from "js-yaml";
+import { load, dump } from "js-yaml";
+
+interface AEP {
+  title: string;
+  id: string;
+
+  contents: string;
+}
+
+
 
 const AEP_LOC = process.env.AEP_LOCATION!;
 
@@ -24,18 +33,42 @@ function readAEP(dirPath: string): string[] {
     return [md_contents, yaml_text];
 }
 
-function createMarkdown(files: string[]) {
+function createMarkdown(files: string[]): AEP {
   const md_text = files[0];
   const yaml_text = files[1];
 
   const yaml = load(yaml_text);
+
+  // Add title to yaml
+  var title_regex = /^# (.*)\n/
+  const matches = md_text.match(title_regex);
+  const title = matches[1]!;
+
+  yaml.title = title;
+
+  // Write everything to a markdown file.
+  return {
+    title: title,
+    id: yaml.id,
+    contents: `---
+${dump(yaml)}
+--- 
+${md_text}`
+  }
+}
+
+function writeMarkdown(aep: AEP) {
+  const filePath = path.join("../src/content/docs", `${aep.id}.md`)
+  console.log(filePath);
+  // fs.writeFileSync(filePath, aep.contents);
 }
 
 const aep_folders = await getFolders(path.join(AEP_LOC, "aep/general/"));
 for(var folder of aep_folders) {
   try {
     const files = readAEP(folder);
-    createMarkdown(files);
+    const new_file = createMarkdown(files);
+    writeMarkdown(new_file);
   }
   catch(e) {
     console.log(`AEP ${folder} failed with error ${e}`)
