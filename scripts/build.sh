ae6e83b8510d5833443dfa4f07de4cf360125b76
Alex Stephen <astephen2@gmail.com>
2025-11-04 23:45:38 +0530

    Add scripts/build.sh changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,7 @@ export AEP_LOCATION="/tmp/aeps"
 export AEP_LINTER_LOC="/tmp/api-linter"
 export AEP_OPENAPI_LINTER_LOC="/tmp/aep-openapi-linter"
 export AEP_COMPONENTS_LOC="/tmp/aep-components"
+export AEP_EDITION_2026="/tmp/aeps-2026"
 
 if [ ! -d "${AEP_LOCATION}" ]; then
     git clone https://github.com/aep-dev/aeps.git "${AEP_LOCATION}"
@@ -20,6 +21,11 @@ fi
 
 if [ ! -d "${AEP_COMPONENTS}" ]; then
     git clone https://github.com/aep-dev/aep-components.git "${AEP_COMPONENTS_LOC}"
+fi
+
+if [ ! -d "${AEP_EDITION_2026}" ]; then
+    git clone https://github.com/aep-dev/aeps.git "${AEP_EDITION_2026}"
+    # TODO: Checkout the repo at a specific tag or branch for the 2026 edition
 fi
 
 npm install
