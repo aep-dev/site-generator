dd954e6b50952eb5d4d8c89b79ca2e2405c84048
Alex Stephen <1325798+rambleraptor@users.noreply.github.com>
2025-11-04 23:24:34 +0530

    Edition support!
    Slugs are being used, which gives us full paths
    
    AepList now works properly
    
    Version selector works properly
    
    Alright, just have to get the sidebar looking correct
    
    Sidebar looks correct
    
    prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ import tailwindcss from "@tailwindcss/vite";
 let sidebar = JSON.parse(fs.readFileSync("generated/sidebar.json"));
 let redirects = JSON.parse(fs.readFileSync("generated/redirects.json"));
 let config = JSON.parse(fs.readFileSync("generated/config.json"));
+let aepEditions = JSON.parse(fs.readFileSync("aep-editions.json"));
 
 // https://astro.build/config
 export default defineConfig({
@@ -31,7 +32,26 @@ export default defineConfig({
       ],
       plugins: [
         starlightBlog({ navigation: "none" }),
-        starlightSidebarTopics(sidebar, { exclude: ["/blog", "/blog/**/*"] }),
+        starlightSidebarTopics(sidebar, {
+          topics: {
+            aeps: aepEditions.editions
+              .filter((edition) => edition.folder !== ".")
+              .flatMap((edition) => [
+                `/${edition.folder}`,
+                `/${edition.folder}/**/*`,
+              ]),
+          },
+          exclude: [
+            "/blog",
+            "/blog/**/*",
+            ...aepEditions.editions
+              .filter((edition) => edition.folder !== ".")
+              .flatMap((edition) => [
+                `/${edition.folder}`,
+                `/${edition.folder}/**/*`,
+              ]),
+          ],
+        }),
       ],
       social: [
         { icon: "github", label: "GitHub", href: config.urls.repo },
@@ -47,9 +67,11 @@ export default defineConfig({
         },
       ],
       components: {
+        Banner: "./src/components/overrides/Banner.astro",
         Head: "./src/components/overrides/Head.astro",
         SkipLink: "./src/components/overrides/SkipLink.astro",
         TableOfContents: "./src/components/overrides/TableOfContents.astro",
+        ThemeSelect: "./src/components/overrides/ThemeSelect.astro",
       },
     }),
   ],
